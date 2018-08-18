b628db13d3543da0c3cdcb6cc1011e0bbe200857
FarmerRobbie <farmerrobbie@freshsalad.today>
2018-08-18 13:07:42 -0700

    more ugh

--- Adafruit/circuitpython/lib/embed/abort_.c
+++
@@ -1,9 +0,0 @@
-#include <py/runtime.h>
-
-#include "supervisor/shared/translate.h"
-
-NORETURN void abort_(void);
-
-NORETURN void abort_(void) {
-    mp_raise_msg(&mp_type_RuntimeError, translate("abort() called"));
-}
--- Adafruit/circuitpython/ports/atmel-samd/common-hal/audiobusio/__init__.c
+++
@@ -1,1 +0,0 @@
-// No audiobusio module functions.
